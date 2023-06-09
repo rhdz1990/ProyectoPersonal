aa32b95354641c4f1546f666e98ecc64bb9676ce
rhernandezopengate <43422446+rhernandezopengate@users.noreply.github.com>
2023-06-09 12:36:09 -0600

    Agregar archivos de proyecto.

---
+++ WebAppProyectoPersonal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppProyectoPersonal.Startup))]
+namespace WebAppProyectoPersonal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
